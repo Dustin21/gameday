8aa48b05a39e23234d32cd8d1409d5644b2e0ffc
Dustin21 <dustin.bwj@gmail.com>
2014-11-25 23:42:34 -0800

    update

--- R/gameday.R
+++ R/gameday.R
@@ -1,6 +1,8 @@
 #' The gameday Package
 #'
-#' Tell R to find out if your favourite NHL team is playing on a specific date.
+#' Tell R to find out if your favourite NHL team is playing on a specific date,
+#' or gather the scores of all teams on for a specific date. Stay up-to-date
+#' on that hockey pool!
 #'
 #' 
 #'
